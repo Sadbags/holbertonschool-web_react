91541c6c1285096c68f66941c9da8ecd33710698
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 11:56:22 -0400

    added tasks

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import "./App.css";
-import holberton_logo from "./holberton_logo.jpg";
-import { getFullYear, getFooterCopy } from "./utils";
+import './App.css';
+import holberton_logo from './holberton_logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <div className="App-header">
-        <img src={holberton_logo} />
+        <img src={holberton_logo} alt="logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-      <div className="App-footer">
         <p>
-          Copyright {getFullYear()} - {getFooterCopy(true)}
+          Login to access the full dashboard
         </p>
       </div>
-    </>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
   );
 }
 
