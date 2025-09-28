c4eee7bf6a6dc1944367fe540c5120bd4857aa37
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 13:35:59 -0400

    added tasks

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,26 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('App Component', () => {
-  test('renders 2 input elements', () => {
+describe("App Component", () => {
+  test("renders 2 input elements", () => {
     render(<App />);
-    const inputs = screen.getAllByRole('textbox'); // Finds input elements
-    expect(inputs).toHaveLength(1); // Email input is a textbox
-    const passwordInput = screen.getByLabelText(/password/i); // Finds password input
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
   });
 
-  test('renders 2 label elements with the text Email and Password', () => {
+  test("renders 2 label elements with the text Email and Password", () => {
     render(<App />);
-    const emailLabel = screen.getByLabelText(/email/i);
-    const passwordLabel = screen.getByLabelText(/password/i);
+    const emailLabel = screen.getByText(/email/i);
+    const passwordLabel = screen.getByText(/password/i);
+
     expect(emailLabel).toBeInTheDocument();
     expect(passwordLabel).toBeInTheDocument();
   });
 
   test('renders a button with the text "OK"', () => {
     render(<App />);
-    const button = screen.getByRole('button', { name: /ok/i });
+    const button = screen.getByRole("button", { name: /ok/i });
     expect(button).toBeInTheDocument();
   });
 });
