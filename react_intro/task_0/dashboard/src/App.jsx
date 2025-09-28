849b4292c468904582a8cefacae5f5cf3bebc51f
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 13:49:34 -0400

    added tasks

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
 import "./App.css";
-import holberton_logo from "./holberton-logo.jpg";
+import logo from "./assets/holberton-logo.jpg";
 
 function App() {
-    return (
-      <>
-        <div className="App-header">
-          <img src={holberton_logo} />
-          <h1>School dashboard</h1>
-        </div>
-        <div className="App-body">
-          <p>Login to access the full dashboard</p>
-        </div>
-        <div className="App-footer">
-          <p>Copyright 2020 - holberton School</p>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
+      </div>
+    </>
+  );
+}
 
 export default App;
