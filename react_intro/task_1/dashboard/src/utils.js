fad4d67cbf13138246a3824bc7a32864a914a9ec
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 12:55:53 -0400

    added tasks

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,18 +1,15 @@
-// Returns current year
 function getCurrentYear() {
     return new Date().getFullYear();
   }
 
-  // Returns footer text based on isIndex boolean
   function getFooterCopy(isIndex) {
     if (isIndex) {
-      return "Holberton School";  // exact string
+      return "Holberton School";
     } else {
-      return "Holberton School main dashboard";  // exact string
+      return "Holberton School main dashboard";
     }
   }
 
-  // Export functions for Node test runner and React
   module.exports = {
     getCurrentYear,
     getFooterCopy
