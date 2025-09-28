b21d88f25aa6ae2be9056d4f8c98c47294dfb735
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 14:31:17 -0400

    added tasks

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -1,51 +1,12 @@
-import React from 'react';
-import closeIcon from './assets/close-icon.png';
-import { getLatestNotification } from './utils';
+// Notifications.jsx
+const React = require("react");
 
 function Notifications() {
-  const handleClick = () => {
-    console.log('Close button has been clicked');
-  };
-
   return (
-    <div
-      className="notifications"
-      style={{
-        position: 'relative',
-        padding: '10px',
-        border: '1px solid black',
-        marginBottom: '10px',
-      }}
-    >
+    <div className="notification-items">
       <p>Here is the list of notifications</p>
-      <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent">New resume available</li>
-        <li
-          data-priority="urgent"
-          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-        ></li>
-      </ul>
-      <button
-        onClick={handleClick}
-        aria-label="Close"
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          background: 'none',
-          border: 'none',
-          cursor: 'pointer',
-        }}
-      >
-        <img
-          src={closeIcon}
-          alt="Close icon"
-          style={{ width: '10px', height: '10px' }}
-        />
-      </button>
     </div>
   );
 }
 
-export default Notifications;
+module.exports = Notifications;
