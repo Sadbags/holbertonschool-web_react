893d7b83debb7379a6402eb8d81057eff5f04a4e
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 11:13:34 -0400

    added tasks

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -4,19 +4,19 @@ import holberton_logo from "./holberton-logo.jpg";
 
 function App() {
     return (
-        <>
-            <div className="App-header">
-                <img src={holberton_logo} />
-                <h1>School dashboard</h1>
-                <div className="App-body">
-                    <p>Login to access the full dashboard</p>
-                </div>
-                <div className="App-footer">
-                    <p>Copyright 2025 - holberton School</p>
-                </div>
-            </div>
-        </>
+      <>
+        <div className="App-header">
+          <img src={holberton_logo} />
+          <h1>School dashboard</h1>
+        </div>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+        </div>
+        <div className="App-footer">
+          <p>Copyright 2020 - holberton School</p>
+        </div>
+      </>
     );
-}
+  }
 
 export default App;
