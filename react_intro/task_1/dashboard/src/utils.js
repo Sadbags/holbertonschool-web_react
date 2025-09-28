3998866de0a41de3bdfbd5d2f6a76f0a15ec2bff
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-28 12:04:17 -0400

    added tasks

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,17 +1,13 @@
-const getFullYear = () => {
-	let current_year = new Date().getFullYear();
-	return current_year;
-}
+// Return current year
+export const getFullYear = () => {
+    return new Date().getFullYear();
+  };
 
-const getFooterCopy = (isIndex) => {
-	if (isIndex === true) {
-		return "Holberton School";
-	} else {
-		return "Holberton School main dashboard";
-	}
-}
-
-module.exports = {
-	getFooterCopy,
-	getFullYear,
-};
+  // Return footer text depending on isIndex flag
+  export const getFooterCopy = (isIndex) => {
+    if (isIndex === true) {
+      return "Holberton School";
+    } else {
+      return "Holberton School main dashboard";
+    }
+  };
