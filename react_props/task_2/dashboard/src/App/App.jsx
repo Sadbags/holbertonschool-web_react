89b903118ffe35dbf3529a8c386ef5c66521c889
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-10-05 19:55:36 -0400

    added tasks

--- react_props/task_2/dashboard/src/App/App.jsx
+++ react_props/task_2/dashboard/src/App/App.jsx
@@ -6,11 +6,18 @@ import Login from "../Login/Login.jsx";
 import Footer from "../Footer/Footer.jsx";
 
 function App() {
+  // Creamos el array de notificaciones con los 3 elementos requeridos
+  const notificationsList = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: { __html: "<strong>Urgent requirement</strong>" } },
+  ];
+
   return (
     <div className="App">
       <Fragment>
         <div className="root-notifications">
-          <Notifications />
+          <Notifications notifications={notificationsList} />
         </div>
         <Header />
         <div className="red-line" />
