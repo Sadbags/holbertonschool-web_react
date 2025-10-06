d49a56af164bbd4bdcf92fe7b25c96dd6a18a743
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-10-05 20:14:52 -0400

    added tasks

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,32 +1,42 @@
 import PropTypes from "prop-types";
 
 // NotificationItem renders a notification entry with styling based on props.
-function NotificationItem({ type = "default", html, value }) {
-  const itemStyle = {
-    color: type === "urgent" ? "red" : "blue",
-  };
-
-  if (html) {
+function NotificationItem({ type, value, html }) {
+  // Render condicional según el type
+  if (type === "urgent") {
     return (
       <li
-        style={itemStyle}
-        data-notification-type={type}
-        dangerouslySetInnerHTML={html}
-      ></li>
+        style={{ color: "red" }}
+        data-notification-type="urgent"
+        {...(html ? { dangerouslySetInnerHTML: html } : {})}
+      >
+        {!html && value}
+      </li>
     );
   }
 
+  // default
   return (
-    <li style={itemStyle} data-notification-type={type}>
-      {value}
+    <li
+      style={{ color: "blue" }}
+      data-notification-type="default"
+      {...(html ? { dangerouslySetInnerHTML: html } : {})}
+    >
+      {!html && value}
     </li>
   );
 }
 
 NotificationItem.propTypes = {
   type: PropTypes.string,
-  html: PropTypes.shape({ __html: PropTypes.string }),
   value: PropTypes.string,
+  html: PropTypes.shape({ __html: PropTypes.string }),
+};
+
+NotificationItem.defaultProps = {
+  type: "default",
+  value: "",
+  html: null,
 };
 
 export default NotificationItem;
