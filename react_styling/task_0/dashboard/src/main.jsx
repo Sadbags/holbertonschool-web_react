f8ae05a48d41ca9ded15fee0c990613a1d8c1686
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-10-26 19:28:05 -0400

    added task

---
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App"; // cambiar a .jsx si da error
+
+import "@fontsource/roboto/400.css"; // Regular
+import "@fontsource/roboto/500.css"; // Medium
+import "@fontsource/roboto/700.css"; // Bold
+
+import "./main.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
