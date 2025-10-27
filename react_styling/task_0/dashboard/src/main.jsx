7cf4e8430d37aa0034e9faac1150a5bd63c52d51
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-10-26 20:16:23 -0400

    added task

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App/App.jsx";
-
-import "@fontsource/roboto/400.css"; // Regular
-import "@fontsource/roboto/500.css"; // Medium
-import "@fontsource/roboto/700.css"; // Bold
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import "./main.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
